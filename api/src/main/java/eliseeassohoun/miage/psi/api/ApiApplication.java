b6f100f91dd42b73673dd48620a5655ce220685e
Kadjoassohoun <eliseeassohoun@yahoo.fr>
2020-02-22 00:53:06 +0100

    add config for cors policy

--- api/src/main/java/eliseeassohoun/miage/psi/api/ApiApplication.java
+++ api/src/main/java/eliseeassohoun/miage/psi/api/ApiApplication.java
@@ -2,19 +2,28 @@ package eliseeassohoun.miage.psi.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
+
 public class ApiApplication {
-
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(ApiApplication.class);
-		app.setDefaultProperties(Collections
-				.singletonMap("server.port", "3000"));
-		app.run(args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
